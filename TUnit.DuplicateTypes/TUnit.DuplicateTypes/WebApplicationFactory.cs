ab1c3e7061d3941e7d5a5340c3dcc6ac45370c1a
Patrick Williams <pjwilliams2@users.noreply.github.com>
2025-04-18 07:17:07 -0400

    Add files via upload

---
+++ TUnit.DuplicateTypes/TUnit.DuplicateTypes/WebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TUnit.Core.Interfaces;
+
+namespace TUnit.Multiple.Protobuf;
+
+public class WebApplicationFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+    public Task InitializeAsync()
+    {
+        _ = Server;
+
+        return Task.CompletedTask;
+    }
+}
